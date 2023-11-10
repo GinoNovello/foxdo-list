c274ccad86370563eb7003283f118e535fd16c84
GinoNovello <ginonovello9@gmail.com>
2023-11-10 19:44:47 -0300

    change button name

--- src/i18n/translates.ts
+++ src/i18n/translates.ts
@@ -1,13 +1,13 @@
 export const Translates = {
     ES: {
         defaulttitle: "Título",
-        addtask: "Añadir tarea",
+        addtask: "Añadir",
         addtask_below: "Añade una tarea debajo",
         write_task: "Escribe tu tarea...",
     },
     EN: {
         defaulttitle: "Title",
-        addtask: "Add task",
+        addtask: "Add",
         addtask_below: "Add a task below",
         write_task: "Write your task...",
     },
